ffe12436449797d2755e46e8a6f3604d7f6248ce
Martin Lauf <lauf.martin@gmail.com>
2019-12-19 10:19:37 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+
+// require and use "multer"...
+var multer  = require('multer')
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+//posting file
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res){
+  
+   res.json({
+    'name' : req.file.originalname,
+    'type' : req.file.mimetype,
+    'size' : req.file.size
+   });
+  
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
